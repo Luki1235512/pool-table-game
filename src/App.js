7ac827087e3220a73e31a218f0e89c9a6403cc01
Luki1235512 <Luki1235512@gmail.com>
2022-02-17 17:39:21 +0100

    replaced deprecated react-three-fiber

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Canvas } from 'react-three-fiber';
+import { Canvas } from '@react-three/fiber';
 import Scene from "./views/Scene";
 import Controls from "./components/Controls";
 
