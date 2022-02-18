c7fd4ca620f65856e45333451b131f26071d1921
Luki1235512 <Luki1235512@gmail.com>
2022-02-18 16:53:27 +0100

    update on ball

--- src/views/Scene.js
+++ src/views/Scene.js
@@ -3,6 +3,24 @@ import {useThree} from "@react-three/fiber";
 import Lights from "../components/Lights";
 import PoolTable from "../components/PoolTable";
 
+import zero from '../assets/textures/0.png'
+import one from '../assets/textures/1.png'
+import two from '../assets/textures/2.png'
+import three from '../assets/textures/3.png'
+import four from '../assets/textures/4.png'
+import five from '../assets/textures/5.png'
+import six from '../assets/textures/6.png'
+import seven from '../assets/textures/7.png'
+import eight from '../assets/textures/8.png'
+import nine from '../assets/textures/9.png'
+import ten from '../assets/textures/10.png'
+import eleven from '../assets/textures/11.png'
+import twelve from '../assets/textures/12.png'
+import thirteen from '../assets/textures/13.png'
+import fourteen from '../assets/textures/14.png'
+import fifteen from '../assets/textures/15.png'
+import PoolBall from "../components/PoolBall";
+
 function Scene() {
 
     const {camera} = useThree()
@@ -46,6 +64,24 @@ function Scene() {
             <React.Suspense fallback={<mesh />}>
                 <PoolTable />
             </React.Suspense>
+            <object3D>
+                <PoolBall position={[0, -16, 0]} textureURL={zero} />
+                <PoolBall position={[-1.01, 15, 0]} textureURL={one} />
+                <PoolBall position={[1.01, 17, 0]} textureURL={two} />
+                <PoolBall position={[-0.51, 16, 0]} textureURL={three} />
+                <PoolBall position={[-1.01, 17, 0]} textureURL={four} />
+                <PoolBall position={[-2.02, 17, 0]} textureURL={five} />
+                <PoolBall position={[1.53, 16, 0]} textureURL={six} />
+                <PoolBall position={[0.51, 14, 0]} textureURL={seven} />
+                <PoolBall position={[0, 15, 0]} textureURL={eight} />
+                <PoolBall position={[0, 13, 0]} textureURL={nine} />
+                <PoolBall position={[0.51, 16, 0]} textureURL={ten} />
+                <PoolBall position={[2.02, 17, 0]} textureURL={eleven} />
+                <PoolBall position={[-0.51, 14, 0]} textureURL={twelve} />
+                <PoolBall position={[0, 17, 0]} textureURL={thirteen} />
+                <PoolBall position={[-1.53, 16, 0]} textureURL={fourteen} />
+                <PoolBall position={[1.01, 15, 0]} textureURL={fifteen} />
+            </object3D>
         </>
     );
 }
